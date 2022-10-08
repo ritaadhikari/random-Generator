e9bb0ab0dab3596aaca429ec0268360a5994640b
Rita adhikari <ritaadhikari936@gmail.com>
2022-10-08 19:00:46 +0545

    tis is my random generator

---
+++ script.js
@@ -0,0 +1,18 @@
+let btnRandom = document.querySelector('button');
+let result = document.querySelector('h1');
+
+
+let users = ['Rita','Samikshya','salipa','susmita','dikshya'];
+
+function getRandomNumber(min, max) {
+    let step1 = max - min + 1;
+    let step2 = Math.random() * step1;
+    let result = Math.floor(step2) + min;
+
+    return result;
+}
+
+btnRandom.addEventListener('click', () => {
+    let index = getRandomNumber(0, users.length-1);
+    result.innerText = users[index];
+});
